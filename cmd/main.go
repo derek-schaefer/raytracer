f3c5d9c5589958d9fc9e93e23339a7a89187af43
Derek Schaefer <derek.schaefer@gmail.com>
2024-01-22 22:29:54 +0000

    Refactor main function

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,24 @@ import (
 	"github.com/derek-schaefer/raytracer"
 )
 
+const imageWidth = 256
+
 func main() {
-	fmt.Print(raytracer.Image{Width: 256, Height: 256}.WritePPM())
+	img := raytracer.NewImage(imageWidth, imageWidth)
+
+	for j := 0; j < img.Height; j++ {
+		for i := 0; i < img.Width; i++ {
+			c := raytracer.NewColor(
+				raytracer.Vec3{
+					float64(i) / (float64(img.Width) - 1),
+					float64(j) / (float64(img.Height) - 1),
+					0,
+				},
+			)
+
+			img.Set(c, i, j)
+		}
+	}
+
+	fmt.Print(img.WritePPM())
 }
