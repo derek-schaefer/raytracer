86e8cde4d8683234ea6e791fe64dd9048ffd22a2
Derek Schaefer <derek.schaefer@gmail.com>
2024-01-25 16:33:08 +0000

    Add ray type string implementation

--- ray.go
+++ ray.go
@@ -1,10 +1,17 @@
 package raytracer
 
+import "fmt"
+
 type Ray struct {
 	Origin    Point3
 	Direction Vec3
 }
 
+// Return the point on the ray at the specified value.
 func (r Ray) At(t float64) Point3 {
 	return r.Origin.Add(r.Direction.Multiply(t))
+}
+
+func (r Ray) String() string {
+	return fmt.Sprintf("Ray(%s, %s)", r.Origin, r.Direction)
 }
--- ray_test.go
+++ ray_test.go
@@ -3,13 +3,13 @@ package raytracer_test
 import (
 	"testing"
 
-	"github.com/derek-schaefer/raytracer"
+	r "github.com/derek-schaefer/raytracer"
 )
 
 func TestRayAt(t *testing.T) {
-	r := raytracer.Ray{
-		raytracer.Point3{1, 1, 1},
-		raytracer.Vec3{2, 2, 2},
+	r := r.Ray{
+		r.Point3{1, 1, 1},
+		r.Vec3{2, 2, 2},
 	}
 
 	p := r.At(2)
@@ -23,6 +23,17 @@ func TestRayAt(t *testing.T) {
 	}
 
 	if p.Z() != 5 {
+		t.Fail()
+	}
+}
+
+func TestRayString(t *testing.T) {
+	r := r.Ray{
+		r.Point3{1, 1, 1},
+		r.Vec3{2, 2, 2},
+	}
+
+	if r.String() != "Ray(Vec3(1.000000, 1.000000, 1.000000), Vec3(2.000000, 2.000000, 2.000000))" {
 		t.Fail()
 	}
 }
